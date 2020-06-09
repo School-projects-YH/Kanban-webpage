eb11777643158104e2b1196fbdb0667b0a6da778
Filip Larsson <filipaltdelete@AI.local>
2020-06-09 10:46:50 +0200

    Added api to frontend

--- Frontend/Frontend/Startup.cs
+++ Frontend/Frontend/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Frontend.API;
 
 namespace Frontend
 {
@@ -24,6 +25,8 @@ namespace Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddHttpClient<IApiHandler, ApiHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
