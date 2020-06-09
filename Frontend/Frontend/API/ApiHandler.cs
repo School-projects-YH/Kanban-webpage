d5cdab9144677d3cad0866667f69fbca3617189c
Filip Larsson <filipaltdelete@AI.local>
2020-06-09 14:51:08 +0200

    added functions/connections in apiHandler

--- Frontend/Frontend/API/ApiHandler.cs
+++ Frontend/Frontend/API/ApiHandler.cs
@@ -1,5 +1,13 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Frontend.DTO;
+using System.Collections.Specialized;
 
 namespace Frontend.API
 {
@@ -17,5 +25,31 @@ namespace Frontend.API
         {
             _client = client;
         }
+
+        public async Task<BoardDTO[]> GetBoardsAsync(string url = "http://localhost:9001/api/board")
+        {
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var board = await response.Content.ReadAsAsync<BoardDTO[]>();
+                return board;
+            }
+            return null;
+        }
+
+        public async Task<CardDTO[]> GetCardsAsync(string url = "http://localhost:9000/api/dto")
+        {
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var card = await response.Content.ReadAsAsync<CardDTO[]>();
+                return card;
+            }
+            return null;
+        }
+
+
     }
 }
