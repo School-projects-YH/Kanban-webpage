639c30847a27ab88932ebbbff43face9a19ef525
Lukas Selin <lukas.selin@viskan.se>
2020-06-11 10:00:07 +0200

    Fixed async for DTOController

--- Backend/Backend/Controllers/DTOController.cs
+++ Backend/Backend/Controllers/DTOController.cs
@@ -30,34 +30,31 @@ namespace Backend.Controllers
 
         // GET: api/DTO/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<List<CardDTO>>> GetCard(int id)
+        public async Task<ActionResult<List<CardDTO>>> GetCard(int BoarIid)
         {
             //var card = await _context.Card.FindAsync(id);
 
-            var dtoQuery =  from c in _context.Card
-                           join a in _context.Board on c.BoardId equals a.Id 
-                           join b in _context.Column on c.ColumnId equals b.Id
-                           where a.Id ==id select new 
-                           {
-                                 Id = b.Id,
-                                 Title= a.Title,
-                                 ColumnId = b.Id,
-                                 Info = c.Info
-                           };
+            var dtoQuery = await (from card in _context.Card
+                           join board in _context.Board on card.BoardId equals board.Id
+                           join column in _context.Column on card.ColumnId equals column.Id
+                           where card.Id == BoarIid
+                           select card).ToListAsync();
 
-
-            dtoQuery.ToList();
             if (dtoQuery == null)
             {
                 return NotFound();
             }
+            
             List<CardDTO> dto = new List<CardDTO>();
+            
             foreach (var item in dtoQuery)
-            {       var dtoCard = new CardDTO{  Id= item.Id,Title= item.Title,ColumnId= item.ColumnId,Info= item.Info };
-                    dto.Add(dtoCard);
-            }   
-          return   dto;
-           
+            {
+                var dtoCard = new CardDTO { Id = item.Id, ColumnId = item.ColumnId, Info = item.Info };
+                dto.Add(dtoCard);
+            }
+            
+            return dto;
+
         }
 
         // PUT: api/Card/5
